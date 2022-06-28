6d48ec198a0bf00ba071d7d41827e80bbe10a1a3
Mertcan Cakir <zestillest@gmail.com>
2022-06-28 22:38:35 +0300

    barın hareketine bağlı top yönü fonksiyonu eklendi

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 import keyboard
 import sys
+import time
 
 # normal koordinat düzlemi
 SIZE_Y = 700
 SIZE_X = 500
 background = np.ones((SIZE_Y,SIZE_X))
 clean_background = background.copy()
-cv2.namedWindow("game", cv2.WINDOW_NORMAL) 
-
+cv2.namedWindow("game", cv2.WINDOW_NORMAL)
 # keyboard.is_pressed("q"):
 # genislik = SIZE_X, yükseklik = SIZE_Y
 # |                  |
@@ -28,16 +28,16 @@ class Bar:
     self.start_position = [self.left_side,self.right_side, 495,500]
     self.uses_background = uses_background
     self.up_side = SIZE_Y - 205 # alttan 205 px = 495
-    
+    self.bar_speed = 10
   def movingBar(self, direction = None):
     self.direction = direction
 
     if self.direction == "rigth":
-      self.start_position[0] += 5
-      self.right_side += 5
+      self.start_position[0] += self.bar_speed
+      self.right_side += self.bar_speed
       
-      self.start_position[1] += 5
-      self.left_side += 5
+      self.start_position[1] += self.bar_speed
+      self.left_side += self.bar_speed
       self.direction = None
       if self.right_side >= SIZE_X:
         self.right_side = SIZE_X
@@ -48,11 +48,10 @@ class Bar:
     
       
     elif self.direction == "left":
-      self.start_position[0] -= 5
-      self.right_side -= 5
-
-      self.start_position[1] -= 5
-      self.left_side -= 5
+      self.start_position[0] -= self.bar_speed
+      self.right_side -= self.bar_speed
+      self.start_position[1] -= self.bar_speed
+      self.left_side -= self.bar_speed
       self.direction = None
       if self.right_side <= self.bar_length:
         self.right_side = self.bar_length
@@ -60,8 +59,6 @@ class Bar:
         self.start_position[0] = self.left_side
         self.start_position[1] = self.right_side
     
-    
-      
     return self.start_position
   
 class Ball:
@@ -77,11 +74,12 @@ class Ball:
     self.position_X = 0
     self.position_Y = 0
     self.hit = 0
-    
-  def movingBall(self, bar_status):
+
+  def movingBall(self, bar_status, direction = None):
     self.position_X += self.speed_X
     self.position_Y += self.speed_Y
     self.bar_status = bar_status
+    self.direction = direction
     
     if self.position_X <= 0:
         self.left_status = not self.left_status
@@ -93,6 +91,15 @@ class Ball:
         self.up_status = not self.up_status
     
     if (self.bar_status[0] <= self.position_X <= self.bar_status[1]) and (self.bar_status[2] <= self.position_Y <= self.bar_status[3]) and self.hit == 0:
+        
+        if self.direction == "rigth" and (10 < self.speed_X or self.speed_Y < 10):
+          self.speed_X += 2
+          self.speed_Y -= 2
+          
+        elif self.direction == "left" and (10 < self.speed_X or self.speed_Y < 10):
+          self.speed_X -= 2
+          self.speed_Y += 2
+
         self.hit = not self.hit
         self.speed_Y = -self.speed_Y
 
@@ -114,24 +121,22 @@ bar_status = []
 direction = None
 while True:
   bar_list = bar.movingBar(direction = direction)
-  
-
-
-  if keyboard.is_pressed('d'):
-    direction = "rigth"
-  
-  elif keyboard.is_pressed('a'):
-    direction = "left"
-  else:
-    direction = None
-    
-  bar_list = bar.movingBar(direction = direction)
   bar_x1 = bar_list[0]
   bar_x2 = bar_list[1]
   bar_y1 = bar_list[2]
   bar_y2 = bar_list[3]
+
+  if keyboard.is_pressed('d'):
+
+    direction = "rigth"
   
-  ball_pos_x, ball_pos_y = ball.movingBall(bar_status = bar_list)
+  elif keyboard.is_pressed('a'):
+    direction = "left"
+    
+  else:
+    direction = None
+  
+  ball_pos_x, ball_pos_y = ball.movingBall(bar_status = bar_list, direction = direction)
 
   
   cv2.rectangle(background, (bar_x1, bar_y1), (bar_x2, bar_y2),   (0,0,0), -1)
